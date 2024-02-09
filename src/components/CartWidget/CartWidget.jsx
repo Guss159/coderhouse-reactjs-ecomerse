20c43d24e1755908115e7c18f533dd70d322f670
Gustavo Coniglio <gusta.coniglio@gmail.com>
2024-02-09 12:44:21 -0300

    feat(CartWidget): Implement unmounting behavior when empty

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,13 +1,20 @@
 import cart from "./assets/cart-speed-svgrepo-com.svg";
 import { useCartContext } from "../../context/CartContext";
+import { Link } from "react-router-dom";
 
 const CartWidget = () => {
-	const { totalQuantity } = useCartContext()
-	return (
-		<>
+	const { totalQuantity } = useCartContext();
+
+	const Widget = (
+		<Link to="/cart">
 			<img src={cart} alt="cart-widget" width="100" height="100" />
-			{ totalQuantity }
-		</>
+			{totalQuantity}
+		</Link>
 	);
+
+	const Cart = !(totalQuantity == 0) ? Widget : null;
+
+	return Cart;
 };
+
 export default CartWidget;
