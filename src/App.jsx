ad771da8aabeb6e6e7343bf6bd974b214fd66e52
Gustavo Coniglio <gusta.coniglio@gmail.com>
2024-02-09 04:28:25 -0300

    feat(App): add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-//import "./App.css";
+import "./App.css";
 import NavBar from "./components/Navbar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+	//TODO: add cart and check out
 	return (
 		<div>
 			<BrowserRouter>
