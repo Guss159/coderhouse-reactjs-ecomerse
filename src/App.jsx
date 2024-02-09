7a4d7827bc9a5f53cfe9ccc80ee04b3ff56a02b0
Gustavo Coniglio <gusta.coniglio@gmail.com>
2024-02-09 20:05:20 -0300

    squash: starting Checkout

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailCont
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import CartView from "./components/CartView/CartView";
+import Checkout from "./components/Checkout/Checkout";
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
 						<Route path="/category/:categoryId" element={ <ItemListContainer greeting={"Productos filtrados!"} /> } />
 						<Route path="/item/:itemId" element={<ItemDetailContainer />} />
 						<Route path="/cart" element={<CartView/>} />
+						<Route path='/checkout' element={<Checkout />} />
 						<Route path="*" element={<h1>404 NOT FOUND</h1>} />
 					</Routes>
 			</BrowserRouter>
