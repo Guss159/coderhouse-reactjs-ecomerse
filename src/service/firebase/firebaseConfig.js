8f95008e64472561dc6f610ae44a4965c7187a61
Gustavo Coniglio <gusta.coniglio@gmail.com>
2024-02-09 22:26:24 -0300

    Oopsies: Cant believe i forgot
    Not gonna bother to clean the secrets

--- src/service/firebase/firebaseConfig.js
+++ src/service/firebase/firebaseConfig.js
@@ -1,22 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBO0TUDAYdtIYUgySnY2Bm_6O4p_ChM6wU",
-  authDomain: "ecomerce-coderhouse-5b489.firebaseapp.com",
-  projectId: "ecomerce-coderhouse-5b489",
-  storageBucket: "ecomerce-coderhouse-5b489.appspot.com",
-  messagingSenderId: "569570056849",
-  appId: "1:569570056849:web:7404438b5be1510a647d30"
+	apiKey: import.meta.env.VITE_apiKey,
+	authDomain: import.meta.env.VITE_authDomain,
+	projectId: import.meta.env.VITE_projectId,
+	storageBucket: import.meta.env.VITE_storageBucket,
+	messagingSenderId: import.meta.env.VITE_messagingSenderId,
+	appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // Get the database
-export const db = getFirestore(app)
+export const db = getFirestore(app);
